0518cf6ddbd0f31635c6bb4a6cdf3e02621bd90f
gs1726 <gs1482008@gmail.com>
2026-08-12 04:49:16 +0530

    Add files via upload

---
+++ ac.c
@@ -0,0 +1,26 @@
+/*Q4: Write a program to calculate the area and circumference of a circle given its radius.
+
+/*
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+Area=153.94, Circumference=43.96
+
+Input 2:
+3
+Output 2:
+Area=28.27, Circumference=18.85
+
+*/
+#include <stdio.h>
+int main() 
+{
+    float a,c,r;
+    scanf("%f",&r);
+    a=3.14*r*r;
+    c=2*3.14*r;
+    printf("Area of circle is : 0.4%f\n",a);
+    printf("Circumference of circle is: 0.4%f\n",c);
+    return 0;
+}
