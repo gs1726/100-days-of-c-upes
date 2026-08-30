9f2e22f03e6afa9842cbfe4d0a415fdedd3095a9
psypsy <gs1482008@gmail.com>
2026-08-30 15:02:43 +0530

    Update and rename largestno.c to day8ques16.c

--- day8ques16.c
+++ day8ques16.c
@@ -1,3 +1,18 @@
+//Q16: Write a program to input three numbers and find the largest among them using if–else.
+
+/*
+Sample Test Cases:
+Input 1:
+3 7 5
+Output 1:
+Largest is 7
+
+Input 2:
+-1 -5 0
+Output 2:
+Largest is 0
+
+*/
 #include<stdio.h>
 int main()
 {
