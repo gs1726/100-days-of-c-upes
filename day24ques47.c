05aa0574366bae798fe83d96d9eef90f528a2189
psypsy <gs1482008@gmail.com>
2026-09-02 12:53:06 +0530

    Update and rename pattern1.c to day24ques47.c

--- day24ques47.c
+++ day24ques47.c
@@ -1,3 +1,22 @@
+//Q47: Write a program to print the following pattern:
+/*
+**
+***
+****
+*****
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*
+**
+***
+****
+*****
+
+*/
 #include<stdio.h>
 int main()
 {
