fe28d49f8245680a70abae64d63ec7f2050e16b5
psypsy <gs1482008@gmail.com>
2026-09-02 13:23:34 +0530

    Add files via upload

---
+++ pattern0.c
@@ -0,0 +1,36 @@
+//Q46: Write a program to print the following pattern:
+//*****
+//*****
+//*****
+//*****
+//*****
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+*****
+*****
+*****
+*****
+
+*/
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
