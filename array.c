e29b0e66840aca0df424b236127f6d923480e98d
psypsy <gs1482008@gmail.com>
2026-09-16 12:43:41 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,12 @@
+//class illustration from ppt
+#include<stdio.h>
+int main()
+{
+    int i;
+    int ar[5]={10,20,30,40,50};
+    for(i=0;i<5;i++)
+    {
+        printf("value of ar[%d] is %d\n",i,ar[i]);
+    }
+    return 0;
+}
