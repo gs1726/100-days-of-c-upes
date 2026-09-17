f915d5d73c77e78c6610131c723bcc6667af5c83
psypsy <gs1482008@gmail.com>
2026-09-17 05:50:14 +0530

    Update and rename arrayeo.c to day30ques59.c

--- day30ques59.c
+++ day30ques59.c
@@ -1,3 +1,20 @@
+//Q59: Count even and odd numbers in an array.
+
+/*
+Sample Test Cases:
+Input 1:
+6
+1 2 3 4 5 6
+Output 1:
+Even=3, Odd=3
+
+Input 2:
+4
+2 4 6 8
+Output 2:
+Even=4, Odd=0
+
+*/
 #include<stdio.h>
 void main()
 {
