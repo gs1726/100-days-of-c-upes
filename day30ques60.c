66c918810cb14524e7bfad63b3a09f01246352a2
psypsy <gs1482008@gmail.com>
2026-09-17 05:51:36 +0530

    Add day30ques60.c file

--- day30ques60.c
+++ day30ques60.c
@@ -1,3 +1,14 @@
+//Q60: Count positive, negative, and zero elements in an array.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+-1 0 1 2 -2
+Output 1:
+Positive=2, Negative=2, Zero=1
+
+*/
 #include<stdio.h>
 int main()
 {
