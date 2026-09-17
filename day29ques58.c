4891e9444aa4f233546dadb660e8f41c8622955e
psypsy <gs1482008@gmail.com>
2026-09-17 05:48:19 +0530

    Add day29ques58.c file

--- day29ques58.c
+++ day29ques58.c
@@ -1,3 +1,20 @@
+//Q58: Find the maximum and minimum element in an array.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+2 9 1 4 7
+Output 1:
+Max=9, Min=1
+
+Input 2:
+3
+10 10 10
+Output 2:
+Max=10, Min=10
+
+*/
 // does'nt work for even no elemts arrays
 #include<stdio.h>
 #include<math.h>
